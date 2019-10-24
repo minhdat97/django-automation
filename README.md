275641a26b77bd9b6e86bc177c43c35635ed63ab
Ubuntu <ubuntu@instance-1.us-central1-a.c.web-terraform-ansible.internal>
2019-10-24 10:29:48 +0000

    .gitignore fix

--- README.md
+++
@@ -1,2 +0,0 @@
-# django-automation
-A small project deploy a hello django web using Terraform and Ansible
--- provider.tf
+++
@@ -1,6 +0,0 @@
-# Specify the provider (GCP, AWS, Azure)
-provider "google" {
-credentials = "${file("./creds/web-terraform-ansible-495b7770e1e2.json")}"
-project = "web-terraform-ansible"
-region = "us-central1"
-}
